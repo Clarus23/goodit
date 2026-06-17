ca400274383e98ab08436255180c99886f59e55d
Clarus23 <dnjs3023@gmail.com>
2026-06-17 22:47:32 +0900

    feat: API 공통 응답 포맷 작성

---
+++ src/main/java/kr/goodit/assignment/common/response/ApiResponse.java
@@ -0,0 +1,21 @@
+package kr.goodit.assignment.common.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+    private final boolean success;
+    private final T data;
+    private final String message;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, data, message);
+    }
+}
