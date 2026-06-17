75b50a9b3e240a7d585ecdb6089c868e6c070005
Clarus23 <dnjs3023@gmail.com>
2026-06-18 06:24:52 +0900

    fix: annotation 정리

--- src/main/java/kr/goodit/assignment/member/controller/MyPageController.java
+++ src/main/java/kr/goodit/assignment/member/controller/MyPageController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mypage")
-@RequiredArgsConstructor
 public class MyPageController {
 
     @GetMapping
